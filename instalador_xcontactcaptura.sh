9d655ed267eb6a118fcba668531ee10bdd6978e4
Lucas Chaves <54557869+gothout@users.noreply.github.com>
2024-03-20 15:34:24 -0300

    Update instalador_xcontactcaptura.sh

--- instalador_xcontactcaptura.sh
+++ instalador_xcontactcaptura.sh
@@ -24,7 +24,7 @@ def capture_menu():
         capture_timed()
     elif choice == '3':
         capture_kill()
-    elif choice == '4'
+    elif choice == '4':
         exit(0)
     else:
         print("Opcao invalida.")
